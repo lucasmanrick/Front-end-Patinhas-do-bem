020f3fa621614f029684beac85ded53dd8917d5b
Lucas Manrick <lucasmanrick.ipms@gmail.com>
2024-11-21 19:17:09 -0300

    corrigido nan ao dar like

--- Public/JavaScript/Feed.js
+++ Public/JavaScript/Feed.js
@@ -761,7 +761,7 @@ async function getMostRecentPosts() {
             document.getElementById(`interactContent-${e.ID}`).innerHTML += `
               <div>
                 <div class = "userCommentContent" style="display:flex; align-items:center; gap:5px">
-                  <img style ="border-radius:50%;width:30px;height:30px" src = "https://lucasmanrick.github.io/Front-end-Patinhas-do-bem/${y.IDUsuario}">
+                  <img style ="border-radius:50%;width:30px;height:30px" src = "https://firebasestorage.googleapis.com/v0/b/patinhasdobem-f25f8.appspot.com/o/perfil%2F${y.IDUsuario}?alt=media">
                   <div> <a href="./Users.html" onclick="perfilUser(${y.IDUsuario})" style="text-decoration:none; font-size:13px">${y.Nome}</a></div> 
                 </div>
                <p style=" display:flex;flex-direction:column"><p style="font-size:14px;color:#808080;">${y.Texto}</<p><p style="font-size:10px">${data.toLocaleString('pt-BR')}</<p></p> 
@@ -867,13 +867,12 @@ async function likePost(postID) {
       return response.json();
     })
     .then(async function (myBlob) {
-      let numManyLikes;
+      let numManyLikes = document.getElementById(`manyLikes-${postID}`).textContent
       if (myBlob.success) {
         document.getElementById(`fa-like-${postID}`).classList = []
         document.getElementById(`fa-like-${postID}`).classList = ["fa-solid fa-thumbs-up"]
-        numManyLikes = await parseInt(document.getElementById(`manyLikes-${postID}`).textContent) + 1
+        numManyLikes.trim() === ""? numManyLikes = 1:numManyLikes = parseInt(document.getElementById(`manyLikes-${postID}`).textContent) + 1 
         document.getElementById(`manyLikes-${postID}`).innerHTML = numManyLikes
-        console.log("manylikes")
       } else if (myBlob.error === "você já avaliou este post") {
         numManyLikes = await parseInt(document.getElementById(`manyLikes-${postID}`).textContent) - 1
         document.getElementById(`manyLikes-${postID}`).innerHTML = numManyLikes
